16897a430e16b1b437d8ead7c2d9a223acad56cb
to-y <yitong922@gmail.com>
2016-10-20 22:15:04 +0000

    db migrations and restaurant seeds

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20161020214859) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "food_type"
     t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# puts "Seeding Data ..."
+# Helper functions
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
 
-# Restaurant.destroy_all
+# Only run on development (local) instances not on production, etc.
+raise "Development seeds only (for now)!" unless Rails.env.development?
 
-# 5.times do |i|
-#   Restaurant.create(name: " ##{i}", type: "Sushi", location: "Vancouver")
-# end
+puts "Destroying Data ..."
+
+Restaurant.destroy_all
+Customer.destroy_all
+Reservation.destroy_all
+
+puts "Seeding Data ..."
+
+5.times do |i|
+  Restaurant.create(name: " Restaurant ##{i}", food_type: "Sushi", location: "Vancouver")
+end
+
+5.times do |i|
+  Restaurant.create(name: " Cafe ##{i}", food_type: "Korean", location: "Vancouver")
+end
+
+5.times do |i|
+  Restaurant.create(name: " BBQ ##{i}", food_type: "Chinese", location: "Burnaby")
+end
+
+5.times do |i|
+  Restaurant.create(name: " Hotpot ##{i}", food_type: "Asian", location: "Richmond")
+end
+
+puts "Done!"
