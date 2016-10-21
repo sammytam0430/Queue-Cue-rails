10364374af50a8164afe54de53920be170c1b3f4
Sammy Tam <sammytam2005@hotmail.com>
2016-10-21 04:04:45 +0000

    added first test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:customer) { Customer.new(first_name: 'Mickey', last_name: 'Mouse', email: 'mmouse@disney.com', password: '123456', password_confirmation: '123456') }
+
+  describe 'Validations' do
+
+    it "is valid with valid attributes" do
+      expect(customer).to be_valid
+    end
+
+  end
 end
