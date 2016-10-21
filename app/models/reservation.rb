574058a608b468a1f078de718067b3768b591c7d
Sammy Tam <sammytam2005@hotmail.com>
2016-10-21 21:22:13 +0000

    added numericality in validation for party size

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :restaurants
-  belongs_to :customers 
+  belongs_to :customers
 
-  validates :party_size, presence: true
+  validates :party_size, presence: true, numericality: { in: 1..6 }
 
 end
