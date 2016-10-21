b1daa29b53503b4bb84c6379f88c722ed7924e4f
to-y <yitong922@gmail.com>
2016-10-21 01:19:03 +0000

    added cross origin request and preflight routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :allow_cross_origin_requests, if: proc { Rails.env.development? }
+
+  def preflight
+    render nothing: true
+  end
+
+private
+  def allow_cross_origin_requests
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Max-Age'] = '1728000'
+  end
+
   protect_from_forgery with: :exception
 end
