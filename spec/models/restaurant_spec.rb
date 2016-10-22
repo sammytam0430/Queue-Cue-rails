23ae75daf0724123e3fe06e6afdd0e55e851ec85
Sammy Tam <sammytam2005@hotmail.com>
2016-10-22 04:06:23 +0000

    refactored restaurant spec

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,39 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-
-  let(:restaurant) { Restaurant.new(name: 'Miku', food_type: 'Sushi', location: 'Vancouver') }
-
-  describe 'Validations' do
+  context 'Validations' do
+    let(:restaurant) { Fabricate(:restaurant) }
 
     it "is valid with valid attributes" do
       expect(restaurant).to be_valid
     end
 
     # Name should be required
-    it "is not valid without a name" do
-      restaurant.name = nil
-      expect(restaurant).to be_invalid
-      expect(restaurant.errors.size).to eql(1)
-      expect(restaurant.errors.full_messages).to include 'Name can\'t be blank'
+    context "without a name" do
+      let(:restaurant) { Fabricate.build(:restaurant, name: nil) }
+
+      it "is invalid" do
+        expect(restaurant).to be_invalid
+      end
+      it "has 1 error message" do
+        restaurant.valid?
+        expect(restaurant.errors.size).to eql(1)
+      end
+      it "include 'Name can\'t be blank'" do
+        restaurant.valid?
+        expect(restaurant.errors.full_messages).to include 'Name can\'t be blank'
+      end
+
     end
 
     # Food type should be required
-    it "is not valid without a name" do
-      restaurant.food_type = nil
-      expect(restaurant).to be_invalid
-      expect(restaurant.errors.size).to eql(1)
-      expect(restaurant.errors.full_messages).to include 'Food type can\'t be blank'
+    context "without a name" do
+      let(:restaurant) { Fabricate.build(:restaurant, food_type: nil) }
+
+      it "is invalid" do
+        expect(restaurant).to be_invalid
+      end
+      it "has 1 error message" do
+        restaurant.valid?
+        expect(restaurant.errors.size).to eql(1)
+      end
+      it "include 'Food type can\'t be blank'" do
+        restaurant.valid?
+        expect(restaurant.errors.full_messages).to include 'Food type can\'t be blank'
+      end
+
     end
 
     # Location should be required
-    it "is not valid without a location" do
-      restaurant.location = nil
-      expect(restaurant).to be_invalid
-      expect(restaurant.errors.size).to eql(1)
-      expect(restaurant.errors.full_messages).to include 'Location can\'t be blank'
+    context "without a name" do
+      let(:restaurant) { Fabricate.build(:restaurant, location: nil) }
+
+      it "is invalid" do
+        expect(restaurant).to be_invalid
+      end
+      it "has 1 error message" do
+        restaurant.valid?
+        expect(restaurant.errors.size).to eql(1)
+      end
+      it "include 'Location can\'t be blank'" do
+        restaurant.valid?
+        expect(restaurant.errors.full_messages).to include 'Location can\'t be blank'
+      end
     end
 
   end
-
 end
