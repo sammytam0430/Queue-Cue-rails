9389602d14f305d5d992d2b160b5a2da93748a90
Sammy Tam <sammytam2005@hotmail.com>
2016-10-22 04:23:20 +0000

    created fabricator and refactored spec for reservation

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,45 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-
-  let(:customer) { Customer.new(name: 'Mickey', phone_number: '6041231234', email: 'mmouse@gmail.com') }
-
-  let(:restaurant) { Restaurant.new(name: 'Miku', food_type: 'Sushi', location: 'Vancouver') }
-
-  let(:reservation) {Reservation.new(party_size: '6', restaurant: restaurant, customer: customer)}
-
-  describe 'Validations' do
+  context 'Validations' do
+    let(:reservation) { Fabricate(:reservation) }
 
     it "is valid with valid attributes" do
-      reservation.save
       expect(reservation).to be_valid
     end
 
     # Party size should be required
-    it "is not valid without a party size" do
-      reservation.party_size = nil
-      expect(reservation).to be_invalid
-      expect(reservation.errors.size).to eql(2)
-      expect(reservation.errors.full_messages).to include 'Party size can\'t be blank'
-      expect(reservation.errors.full_messages).to include 'Party size is not a number'
+    context "without a party size" do
+      let!(:reservation) { Fabricate.build(:reservation, party_size: nil) }
+
+      it "is invalid" do
+        expect(reservation).to be_invalid
+      end
+      it "has 2 error message" do
+        reservation.valid?
+        expect(reservation.errors.size).to eql(2)
+      end
+      it "include 'Party size can\'t be blank'" do
+        reservation.valid?
+        expect(reservation.errors.full_messages).to include 'Party size can\'t be blank'
+      end
+      it "include 'Party size is not a number''" do
+        reservation.valid?
+        expect(reservation.errors.full_messages).to include 'Party size is not a number'
+      end
+
     end
 
     # Restaurant should be required
-    it "is not valid without a restaurant" do
-      reservation.restaurant = nil
-      expect(reservation).to be_invalid
-      expect(reservation.errors.size).to eql(2)
-      expect(reservation.errors.full_messages).to include 'Restaurant can\'t be blank'
-      expect(reservation.errors.full_messages).to include 'Restaurant must exist'
+    context "without a restaurant" do
+      let!(:reservation) { Fabricate.build(:reservation, restaurant: nil) }
+
+      it "is invalid" do
+        expect(reservation).to be_invalid
+      end
+      it "has 2 error message" do
+        reservation.valid?
+        expect(reservation.errors.size).to eql(2)
+      end
+      it "include 'Restaurant can\'t be blank'" do
+        reservation.valid?
+        expect(reservation.errors.full_messages).to include 'Restaurant can\'t be blank'
+      end
+      it "include 'Restaurant must exist''" do
+        reservation.valid?
+        expect(reservation.errors.full_messages).to include 'Restaurant must exist'
+      end
+
     end
 
     # Customer should be required
-    it "is not valid without a customer" do
-      reservation.customer = nil
-      expect(reservation).to be_invalid
-      expect(reservation.errors.size).to eql(2)
-      expect(reservation.errors.full_messages).to include 'Customer can\'t be blank'
-      expect(reservation.errors.full_messages).to include 'Customer must exist'
+    context "without a customer" do
+      let!(:reservation) { Fabricate.build(:reservation, customer: nil) }
+
+      it "is invalid" do
+        expect(reservation).to be_invalid
+      end
+      it "has 2 error message" do
+        reservation.valid?
+        expect(reservation.errors.size).to eql(2)
+      end
+      it "include 'Customer can\'t be blank'" do
+        reservation.valid?
+        expect(reservation.errors.full_messages).to include 'Customer can\'t be blank'
+      end
+      it "include 'Customer must exist''" do
+        reservation.valid?
+        expect(reservation.errors.full_messages).to include 'Customer must exist'
+      end
     end
 
   end
