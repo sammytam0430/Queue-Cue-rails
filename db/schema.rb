00e54b07999d11ec77acaf686d2f300a7cba973f
to-y <yitong922@gmail.com>
2016-10-27 22:38:07 +0000

    pull commits

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20161027025029) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
-    t.string   "phone_number"
+    t.integer  "phone_number"
     t.string   "email"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
